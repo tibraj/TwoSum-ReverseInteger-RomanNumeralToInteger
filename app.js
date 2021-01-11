e6ee692fe26624abb584de1121ab526558deb102
tibraj <tibraj94@gmail.com>
2021-01-11 14:28:53 -0500

    two sum

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 //Two Sum
-
+function twoSum(array, target) {
+    const previousValues = {};
+    for(let i= 0; i < array.length; i++) {
+        let currentNumber = array[i];
+        let neededValue = target - currentNumber;
+        let index2 = previousValues[neededValue];
+        if(index2 !== null) {
+            return {index2, 1}
+        } else {
+            previousValues[currentNumber] = i;
+        }
+    }
+}
 //Reverse Integer
 
 //Roman Numeral to Integer
